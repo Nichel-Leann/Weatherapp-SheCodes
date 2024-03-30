26bf900b222b3847d915312b5fe0a5330a9c1115
Nichel-Leann <160755941+Nichel-Leann@users.noreply.github.com>
2024-03-30 18:14:14 -0500

    added basic search feature in JS

---
+++ index.js
@@ -0,0 +1,9 @@
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = searchInput.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchSubmit);
